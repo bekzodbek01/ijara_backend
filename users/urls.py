05a3da568b6143abbcb4e09cdd35eab6bb2f8915
Farhod <farhod@example.com>
2025-06-11 22:54:44 +0500

    2

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('change-password/', UserPasswordChangeView.as_view(), name='user-change-password'),
     path('face-compare/', FaceCompareAPIView.as_view(), name='face-compare'),
-    path('face-compare/', FaceCompareAPIView.as_view(), name='face-compare'),
 ]

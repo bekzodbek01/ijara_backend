bf78cbd20c0aafe4e423240ee932da441aa79aad
Farhod <farhod@example.com>
2025-07-10 23:32:14 +0500

    Local settings update

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-#
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
